85cd62b5fc686a96809b874fb0a23574044f66af
Tranel <tranel@Tranels-MacBook-Air.local>
2023-05-08 14:00:49 -0500

    Fixed major issues in code

--- index.js
+++ index.js
@@ -1,52 +1,54 @@
 const inquirer = require('inquirer');
-const { Circle, Triangle, Square, saveSVG } = require('./lib/shapes');
+const fs = require('fs');
+const { Circle, Triangle, Square } = require('./lib/shapes');
+const shapes = ['circle', 'triangle', 'square']
 
-function generateLogo() {
-    inquirer
-        .prompt([
-            {
-                type: 'input',
-                name: 'text',
-                message: 'Enter up to three characters:',
-                validate: function(value) {
-                    if(value.length > 3) {
-                        return 'Please enter a maximum of three characters.'
-                    }
-                    return true;
-                },
-            },
-            {
-                input: 'input',
-                name: 'textColor',
-                message: 'Enter the text color:',
-            },
-            {
-                type: 'list',
-                name: 'shape',
-                message: 'Select a shape:',
-                choices: ['Circle', 'Triangle', 'Square'],
-            },
-            {
-                type: 'input',
-                name: 'color',
-                message: 'Enter a keyword or a hexidecimal number for the shape color:',
-            },
-        ])
-        .then((answers) => {
-            let shape;
-            if (answers.shape === 'Circle') {
-                shape = new Circle (100);
-            } else if (answers.shape === 'Triangle') {
-                shape = new Triangle(150, 100);
-            } else if (answers.shape === 'Square') {
-                shape = new Square(200);
-            }
-            shape.setText(answers.text);
-            shape.setTextColor(answers.textColor);
-            shape.setColor(answers.color);
-            saveSVG(shape, 'logo.svg');
-            console.log('Succesfully generateed your logo!');
-        });
-}
-    
-generateLogo();
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'text',
+            message: 'Enter up to three characters:',
+        },
+        {
+            input: 'input',
+            name: 'textColor',
+            message: 'Enter the text color:',
+            default: 'black',
+        },
+        {
+            type: 'list',
+            name: 'shape',
+            message: 'Select a shape:',
+            choices: shapes,
+        },
+        {
+            type: 'input',
+            name: 'color',
+            message: 'Enter a keyword or a hexidecimal number for the shape color:',
+            default: 'white',
+        },
+    ])
+    .then((answers) => {
+        let shape;
+
+        switch (answers.shape) {
+            case 'circle':
+                shape = new Circle(answers.color, answers.textColor);
+                break;
+            case 'triangle':
+                shape = new Triangle(answers.color, answers.textColor);
+                break;
+            case 'square':
+                shape = new Square(answers.color, answers.textColor);
+                break;
+            default: 
+                console.log('Invalid shape.');
+        }
+
+        shape.setText(answers.text);
+        const svgString = shape.generateSVG();
+        fs.writeFileSync('logo.svg', svgString);
+        console.log('Your logo.svg has been succesfully generated!');
+    })
+    .catch((err) => console.error(err));
